7798603ead742ceb08924cf93745167570208676
em-kotov <123034420+em-kotov@users.noreply.github.com>
2024-11-25 18:34:52 -0500

    fix

--- Scripts/Utility/RandomExtensions.cs
+++ Scripts/Utility/RandomExtensions.cs
@@ -2,12 +2,6 @@ using UnityEngine;
 
 public static class RandomExtensions
 {
-    public static Vector3 GetRandomPositionXZ(Vector3 position, float radius)
-    {
-        Vector3 randomPositionXZ = Random.insideUnitSphere * radius;
-        return new Vector3(randomPositionXZ.x, position.y, randomPositionXZ.z);
-    }
-
     public static Vector3 GetRandomPosition(Vector3 position, float radius)
     {
         Vector3 randomPosition = Random.insideUnitSphere * radius;
